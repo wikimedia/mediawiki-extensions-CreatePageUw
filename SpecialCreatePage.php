55e90a78bd6173ac3604d3708ce39331af09ca29
Edward Chernenko <edwardspec@gmail.com>
2016-12-24 23:26:51 +0300

    (init) Modern port of Special:CreatePage
    Change-Id: I363fa36049dd1a5cf7e343673fa1481e63cedb4f

---
+++ SpecialCreatePage.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+	@file
+	@brief Implements [[Special:CreatePage]].
+
+	We can link to Special:CreatePage when user asks "Where do I create a new page?".
+
+	Note: this is a modern port of "Uniwiki CreatePage" extension (obsolete in 2012).
+	Please see the original extension for authors.
+*/
+
+class SpecialCreatePage extends FormSpecialPage {
+	public function __construct() {
+		parent::__construct( 'CreatePage', 'createpage' );
+	}
+
+	protected function getFormFields() {
+		return array(
+			'Title' => array(
+				'id' => 'mw-input-wptitle',
+				'type' => 'text',
+				'label-message' => 'createpage-instructions'
+			)
+		);
+	}
+
+	protected function alterForm( HTMLForm $form ) {
+		$form->setWrapperLegendMsg( 'createpage' );
+	}
+
+	public function onSubmit( array $params ) {
+		$out = $this->getOutput();
+
+		$name = $params['Title'];
+		if ( !$name ) {
+			$out->redirect( $this->getTitle()->getFullURL() );
+			return Status::newGood();
+		}
+
+		$title = Title::newFromText( $name );
+		if ( !$title ) {
+			return Status::newFatal( 'badtitletext' );
+		}
+
+		if ( $title->exists() ) {
+			$out->addWikiMsg( 'createpage-titleexists', $title->getFullText() );
+			$out->addHTML( Linker::linkKnown(
+				$title, $out->msg( 'createpage-editexisting' )->plain(),
+				array(),
+				array('action' => 'edit')
+			) );
+			$out->addHTML( Xml::element('br') );
+			$out->addHTML( Linker::linkKnown(
+				$this->getTitle(),
+				$out->msg( 'createpage-tryagain' )->plain()
+			) );
+
+			return Status::newGood();
+		}
+
+		$out->redirect( $title->getEditURL() );
+		return Status::newGood();
+	}
+}
